c26b1dc4eba3474949e4329a03c643a10ba32012
gsellers37 <gsellers@mit.edu>
2017-12-17 16:41:48 -0500

    gsellers day 03 part 2

--- day03/gsellers37.js
+++ day03/gsellers37.js
@@ -31,9 +31,88 @@ function partOne(input){
   m = Math.abs(half_side_len - (difference % (side_len-1)));
 
   output +=m
-  
+
   console.log('Manhattan distance for ' +input +' = ' +output);
 }
 
 
-partOne(361527)
+function grid(n,m,value){
+  if (~value)
+    value = Number(0)
+  xy = {}
+  for(i=-n;i<=n;i++){
+    xy[i] = {}
+    for (j=-m;j<=m;j++){
+        xy[i][j]=value
+    }
+  }
+  return xy
+}
+
+function spiral(input,xy,x,y,dir){
+  if (x==undefined)
+    x=0;
+  if (y==undefined)
+    y=0;
+  if (dir==undefined)
+    dir = [1,0];
+
+  //Check if the spiral should turn, and if so, do it
+  dir = checkDirection(x,y,dir,xy);
+
+  // Calculate the next positions value
+  walkOutput = walk(x,y,dir,xy);
+  x = walkOutput[0]
+  y = walkOutput[1]
+  value = walkOutput[2]
+  xy = walkOutput[3]
+  console.log(dir,xy,x,y)
+
+  // Either end or loop until it ends
+  if (value>input)
+    return [x,y,value,input];
+  else
+    return spiral(input,xy,x,y,dir);
+}
+
+function checkDirection(x,y,dir,xy){
+  newdir = changeDirection(dir);
+  if (xy[x+newdir[0]][y+newdir[1]] == 0){
+    return newdir;}
+  else {
+    return dir;
+  }
+}
+
+function changeDirection(dir){
+
+  if (dir[0] == 1 & dir[1] == 0)
+    return [0,1]
+  else if (dir[0] == 0 & dir[1] == 1)
+    return [-1,0]
+  else if (dir[0] == -1 & dir[1] == 0)
+    return [0, -1]
+  else if (dir[0] == 0 & dir[1] == -1)
+    return [1,0]
+  else {
+    console.log('something broke in change direction')
+  }
+}
+
+function walk(x,y,dir,xy){
+  newx = x+dir[0]
+  newy = y+dir[1]
+  for (i=-1;i<=1;i++){
+    for (j=-1;j<=1;j++){
+      if (i==0 && j==0)
+        continue
+      xy[newx][newy]+=xy[newx+i][newy+j]
+    }
+  }
+  return [newx,newy,xy[newx][newy],xy]
+}
+
+var xy = grid(10,10);
+xy[0][0] = 1;
+
+console.log('Part 2 = ',spiral(361527));
